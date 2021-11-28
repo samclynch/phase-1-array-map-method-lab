6800db8889a62c0d532e5889e0846058438d39a4
Sam Lynch <samdawkins1998@gmail.com>
2021-11-27 21:12:24 -0500

    commit tree 7110ab796c667c1b34c7c1da06b2a1404861f67b

--- index.js
+++ index.js
@@ -11,6 +11,51 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = (arr) => {
+//   const tutorials = arr.filter(tutorials => tutorials.toLowerCase.split('').map());
+//   return tutorials
+// }
+ 
+
+
+// function titleCased(tutorials){
+//   return tutorials.toLowerCase().split(" ").map(function(word) {
+//     return word.charAt(word[0], +word[0].toTitleCase());
+//   }).join();
+// }
+// titleCased(tutorials)
+
+
+// get array of all titles
+// const newArr = tutorials.map((tutorials) => tutorials.split(' ')[0])
+// console.log(newArr);
+ let tCasedTitles = [];
+
+
+
+
+//  function titleCased(tutorials){
+//    let splitStr=tutorials.map(function(string){
+//      return string.split(" ");
+//    })
+//    splitStr.map(function(array){
+//      let toAdd = array.map(function(word){
+//        return word.charAt(0).toUpperCase() + word.slice(1)
+//      })
+//      return tCasedTitles.push(toAdd)
+//      })
+//      let titleCased = tCasedTitles.map(function(array){
+//        return array.join(" ")
+//    })
+//    return titleCased
+//  }
+
+//  console.log(titleCased(tutorials))
+
+
 const titleCased = () => {
-  return tutorials
+  const array = tutorials.map(element => 
+    element.split(' ').map(word => word.charAt(0).toUpperCase()+word.substring(1)).join(' ')
+    )
+    return array;
 }
